f63921316332295166d4aac5e89a5101c9656de3
Carlos Estrada <cestrada1109@gmail.com>
2022-10-05 01:44:54 -0500

    Add database connection

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const connectDB = require('./config/database')
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+// Database connection 
+connectDB();
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const connectDB = require('./config/database');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
